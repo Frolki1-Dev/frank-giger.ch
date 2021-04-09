565a5b4b07bee6054965339c0434b4b12030780e
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2021-04-09 10:41:11 +0200

    Make it static

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,7 @@ let mix = require('laravel-mix');
 
 mix.setPublicPath('assets');
 
-mix.sass('resources/sass/app.scss', 'app.css');
+mix.sass('resources/sass/app.scss', 'app.css').options({
+    processCssUrls: false
+});
 mix.js('resources/js/app.js', 'app.js');
