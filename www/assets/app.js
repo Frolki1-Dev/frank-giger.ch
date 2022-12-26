ae9acc3aa93cf76b6b0213a5e60e160599db5739
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2022-12-27 00:11:26 +0100

    fix ios bug

--- www/assets/app.js
+++ www/assets/app.js
@@ -7,7 +7,14 @@
   \*****************************/
 /***/ (() => {
 
-
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  /* iOS hides Safari address bar */
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      window.scrollTo(0, 1);
+    }, 1000);
+  });
+}
 
 /***/ }),
 
