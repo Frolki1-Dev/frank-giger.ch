d054f21fd62ff94437e0e22cbe4b20c5a237fa39
Frank Giger <18479991+Frolki1-Dev@users.noreply.github.com>
2022-12-26 23:35:25 +0100

    My new version of website

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,9 @@
 let mix = require('laravel-mix');
 
-mix.setPublicPath('assets');
+mix.setPublicPath('www/assets');
 
 mix.sass('resources/sass/app.scss', 'app.css').options({
     processCssUrls: false
 });
 mix.js('resources/js/app.js', 'app.js');
+mix.disableNotifications();
